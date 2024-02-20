778f8f7e8a3f97350aff2431de257fe7a9867e70
ayoola ajiboye <callcarliecode@yahoo.co.uk>
2024-02-20 17:22:25 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from github_data_transformer import Github_Data_Transformer
-
-handler = Github_Data_Transformer()
-
-handler.extract_github_data()
-handler.clean_transform_data()
-handler.save_as_parquet()
