4df0ba50fd704a88f0b70c9af404808cd0cc63dd
ayoola ajiboye <callcarliecode@yahoo.co.uk>
2024-02-20 17:32:22 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,5 +1,5 @@
 project_configs = {
-    "token" : "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",
+    "token" : "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",
     "org_name" : "Scytale-exercise",
     "spark_master" : "http://127.0.0.1:32776/"
 }
